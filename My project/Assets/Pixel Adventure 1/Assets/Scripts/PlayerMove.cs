48c9ecec4766cfac5e516f767249c77b91c03028
Mauren-Nieto-UNI <mbnietor@unadvirtual.edu.co>
2024-05-03 23:29:37 -0500

    Mejora de salto y giro del personaje en la dirección deseada.

--- My project/Assets/Pixel Adventure 1/Assets/Scripts/PlayerMove.cs
+++ My project/Assets/Pixel Adventure 1/Assets/Scripts/PlayerMove.cs
@@ -11,6 +11,11 @@ public class PlayerMove : MonoBehaviour
 
     Rigidbody2D rb2D;
 
+    public bool betterJump = false;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
+    public SpriteRenderer spriteRendere;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +31,12 @@ public class PlayerMove : MonoBehaviour
        if(UnityEngine.Input.GetKey("d") || UnityEngine.Input.GetKey("right"))
        {
         rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+        spriteRendere.flipX = false;
        } 
        else if (UnityEngine.Input.GetKey("a") || UnityEngine.Input.GetKey("left"))
        {
         rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+        spriteRendere.flipX = true;
        }
        else
        {
@@ -40,6 +47,18 @@ public class PlayerMove : MonoBehaviour
        if (UnityEngine.Input.GetKey("space") && CheckGround.isGrounded)
        {
         rb2D.velocity = new Vector2(rb2D.velocity.x,jumpSpeed);
+       }
+
+       if (betterJump)
+       {
+        if (rb2D.velocity.y<0)
+        {
+            rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+        }
+        if(rb2D.velocity.y>0 && !UnityEngine.Input.GetKey("space"))
+        {
+            rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+        }
        }
     }
 }
