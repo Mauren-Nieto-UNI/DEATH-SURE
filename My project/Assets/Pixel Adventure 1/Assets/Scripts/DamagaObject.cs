f1184e786544f146eb7e850af2d8f32056457bbd
Mauren-Nieto-UNI <mbnietor@unadvirtual.edu.co>
2024-05-04 18:49:54 -0500

    Integración de trampas y muerte del personaje

---
+++ My project/Assets/Pixel Adventure 1/Assets/Scripts/DamagaObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DamagaObject : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            UnityEngine.Debug.Log("Player Damaga");
+            Destroy(collision.gameObject);
+        }
+    }
+}
